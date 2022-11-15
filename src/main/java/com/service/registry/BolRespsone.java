32f59a589362187d29323b67aabceb9ff67442b5
Shailendra1000 <shamal1@mail.sfsu.edu>
2022-11-15 02:00:30 -0500

    done

---
+++ src/main/java/com/service/registry/BolRespsone.java
@@ -0,0 +1,14 @@
+package com.service.registry;
+
+public class BolRespsone {
+
+	public static void main(String[] args) {
+		System.out.println("This is for the Bol");
+		
+		try {
+			System.out.println("catchone");
+		} catch (Exception e) {
+			System.out.println("print one");		}
+	}
+
+}
