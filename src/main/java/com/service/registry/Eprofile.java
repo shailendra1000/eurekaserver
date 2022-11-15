577ebffaafe75a8595bb86953b2025dd62cfee7c
Shailendra1000 <shamal1@mail.sfsu.edu>
2022-11-15 01:38:46 -0500

    commn1. done eprofile part

---
+++ src/main/java/com/service/registry/Eprofile.java
@@ -0,0 +1,21 @@
+package com.service.registry;
+
+public class Eprofile {
+
+	public static void main(String[] args) {
+		System.out.println("this is to get the epro repsone");
+
+	}
+
+	
+	public void version() {
+		
+		try {
+			System.out.println("there rae things to learn");
+			
+		} catch (Exception e) {
+			System.out.println("you will fail in life");
+		}
+		
+	}
+}
